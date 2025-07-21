365dea7b0aaa3354e8b8e155c47f7193566513f3
Bhuvan1104 <bhuvanbodapatla@gmail.com>
2025-07-21 15:54:07 +0530

    dfds

---
+++ add.py
@@ -0,0 +1,24 @@
+# calculator.py
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+if __name__ == "__main__":
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+
+    print(f"Addition: {add(a, b)}")
+    print(f"Subtraction: {sub(a, b)}")
+    print(f"Multiplication: {mul(a, b)}")
+    print(f"Division: {div(a, b)}")
